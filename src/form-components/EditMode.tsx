94e0a9c741bc28aa6868721d97356db46b613b43
codeBodger <diesel.ack+github@gmail.com>
2025-03-16 22:56:04 -0400

    Wrote EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                checked={editMode}
+                onChange={() => {
+                    setEditMode(!editMode);
+                }}
+            />
+            {editMode ?
+                <Form.Group>
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control value={name} onChange={updateName} />
+                    <Form.Check
+                        type="checkbox"
+                        id="student"
+                        label="Student?"
+                        checked={student}
+                        onChange={() => {
+                            setStudent(!student);
+                        }}
+                    />
+                </Form.Group>
+            :   <div>
+                    {name} is {student ? "" : "not "}a student
+                </div>
+            }
         </div>
     );
 }
